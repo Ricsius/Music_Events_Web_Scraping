0b6ce8293df4071a5918263d7d6d4ecce7693c8e
Ricsius <hajnal.ricsi@gmail.com>
2024-12-02 14:34:45 +0100

    Add event storage

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 import selectorlib
+import os.path
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 YAML_PATH = "extract.yaml"
+EVENTS_PATH = "data.txt"
 
 def scrape(url):
     response = requests.get(url, headers=HEADERS)
@@ -18,8 +20,28 @@ def extract(source):
 
     return value
 
+def send_email():
+    print("Email was sent")
+
+def store_event(event):
+    with open(EVENTS_PATH, "a") as file:
+        file.write(event + "\n")
+
+def read_events():
+    if not os.path.isfile(EVENTS_PATH):
+        return []
+    
+    with open(EVENTS_PATH, "r") as file:
+        events = [event.strip() for event in file.readlines()]
+
+    return events
 
 scraped = scrape(URL)
 extracted = extract(scraped)
 
-print(extracted)
+if extracted != "No upcoming tours":
+    events = read_events()
+
+    if extracted not in events:
+        send_email()
+        store_event(extracted)
